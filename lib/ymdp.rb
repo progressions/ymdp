8c4494f84b5953883a837a272dfb6e66fe55c265
Jeff Coleman <progressions@gmail.com>
2010-01-10 12:12:14 -0600

    change ymdp.rb

--- lib/ymdp.rb
+++ lib/ymdp.rb
@@ -1,29 +1,17 @@
-require 'fileutils'
-require 'erb'
-require 'net/http'
-require 'optparse'
-
-require 'vendor/gems/environment'
-Bundler.require_env
-
-require 'constants'
-
 # load application
 #
-Dir["#{APPLICATION_PATH}/*.rb"].each do |path|
+Dir["lib/application_view/*.rb"].each do |path|
   require path
 end
 
 # load support
 #
-Dir["#{APPLICATION_PATH}/support/*.rb"].each do |path|
+Dir["lib/application_view/support/*.rb"].each do |path|
   require path
 end
 
 # load everything in the lib directory
 #
-Dir["#{BASE_PATH}/lib/**/*.rb"].each do |path|
+Dir["lib/**/*.rb"].each do |path|
   require path unless path == File.expand_path(__FILE__)
 end
-
-require "#{APPLICATION_PATH}/support/file"
